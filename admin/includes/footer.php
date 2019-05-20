40f78937a100858d6d357a5ebd03d66a38fda33b
Md Rashed Hossain <safanahmed5@gmail.com>
2019-05-20 16:13:19 +0600

    code beaurified

--- admin/includes/footer.php
+++ admin/includes/footer.php
@@ -1,10 +1,9 @@
-   <section class="footer-section">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-12">
-                   &copy; 2019 Online Library Management System</a> 
-                </div>
-
-            </div>
-        </div>
-    </section>
+<section class="footer-section">
+   <div class="container">
+      <div class="row">
+         <div class="col-md-12">
+            &copy; 2019 Online Library Management System</a> 
+         </div>
+      </div>
+   </div>
+</section>
